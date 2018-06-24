fceb56eb5db47a9659c60e6b3f3a50afeb984d79
Foued <Foued@FOUED-PC>
2018-06-24 18:27:47 +0200

    Ajoutez des fichiers projet.

---
+++ Ex3Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ex3Library.Startup))]
+namespace Ex3Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
